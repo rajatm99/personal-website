60153789b3bc0cfbf7442d1e35332ca548975629
rajatm99 <iamrajatm99@gmail.com>
2024-12-13 00:01:02 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    Rajat Murikkoli
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #000000;
+  color: #008F11;
+
+}
+
+
+#app {
+  font-family: 'Press Start 2P';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
